81b9bc717a128752c13c45609465703833284506
gurdzain <gurdzain@163.com>
2015-06-17 17:00:30 +0800

    wooyun爬虫第一版

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 ###每日一爬虫
 ####1.乌云漏洞精华爬虫
 　　这个爬虫的目的是爬取乌云上面关注数大于10的漏洞，并以好看的样式展现出来。<br />
-请戳[这里](https://github.com/GurdZain/Daily_spider/blob/master/spider/wooyun_spider.py)
+现如今的功能是形成一个列表，然后链接到漏洞页面。等着完成一个离线版。<br />
+请戳[这里](https://github.com/GurdZain/Daily_spider/blob/master/spider/wooyun_spider.py)。
 ####2.乌云知识库分类爬虫
 ####3.乌云zone精华爬虫
 ####4.豆瓣热点电影爬虫
--- spider/wooyun_spider.py
+++ spider/wooyun_spider.py
@@ -3,6 +3,11 @@
 
 import requests
 import re
+import sys
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
 
 headers = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
@@ -21,20 +26,37 @@ pattern = re.compile(reg)
 match = pattern.search(r.text)
 page = int(match.group(1))
 
+# 生成一个html文件来保存
+bug_file = open('bug.html', 'w+')
+bug_file.write(r'<html><head>')
+bug_file.write(r'<meta http-equiv="content-type" content="text/html; charset=utf-8" /><br />')
+bug_file.write(r'<style>a:link {color: #FF4500; text-decoration:none;}'
+               r'a:active:{color: red; text-decoration:underline;}'
+               r'a:visited {color:black;text-decoration:none;}'
+               r'a:hover {color: blue; text-decoration:underline;}'
+               r'body{ font:12px "Trebuchet MS", Arial, Helvetica, sans-serif;}</style>')
+bug_file.write(r"<title>WooYun' s Bug List!!</title>")
+bug_file.write(r'</head>')
+bug_file.write(r"<h2 style='text-align:center;font-size:27px'>WooYun' s Bug List!!<h2><br />")
+
 # 获取每一个bug的信息
-for x in xrange(1, 2):
-    r = requests.get(url % 2, headers=headers)
+for x in xrange(1, 6):
+    r = requests.get(url % x, headers=headers)
     reg2 = r'<tr>\s*<th>.*</th>\s*<td>' \
            r'<a href="(.*)">(.*)</a>\s*.*\s*</td>\s*<th>' \
            r'<a title=".*".*">(\d*)/(\d*)</a></th>\s*<th>' \
            r'<a title=".*" href=".*">(.*)</a></th>\s*</tr>'
     pattern = re.compile(reg2)
     match = pattern.findall(r.text)
-    for i in xrange(0, len(match[x])):
+    for i in xrange(0, len(match)):
         bug_url = match[i][0]
         bug_title = match[i][1]
         bug_comment = match[i][2]
-        bug_concern = match[i][3]
+        bug_concern = int(match[i][3])
         bug_author = match[i][4]
-        if bug_concern >= 10:
-            print(bug_title)
+        if bug_concern > 10:
+            bug_url = r'http://wooyun.org' + bug_url
+            bug = r'<a class="link" href="%s" target="_blank">%s</a>' % (bug_url, bug_title)
+            bug_file.write(bug)
+            bug_file.write('<br />')
+bug_file.write(r'</html>')
